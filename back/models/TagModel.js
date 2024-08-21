651b258e93c22ad54cbf8a649fab2df5dad7b09e
RevinFedor <fr75793@gmail.com>
2024-08-21 19:56:51 +0500

    Удалена папка back из отслеживания Git

--- back/models/TagModel.js
+++
@@ -1,9 +0,0 @@
-// models/TagModel.js
-const mongoose = require('mongoose');
-
-const TagSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true },
-    createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Tag', TagSchema);
